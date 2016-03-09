fc65acc7e4b0e2136570f7b15b81ab5caa9d7b17
supersecretjames <james@backerkit.com>
2016-03-09 02:27:13 +0000

    add flamebomb

--- custom_plugins/dragon.rb
+++ custom_plugins/dragon.rb
@@ -6,10 +6,18 @@ module Lita
         'flame NAME' => 'flamess NAME'
       })
 
+        route(/^flamebomb\s+(.+)/i, :bomb, command: false, help: {
+        'flamebomb me' => 'LIGHTS THE WORLD ON FIRE'
+      })
+      
       def flame(response)
         name = response.matches[0][0]
         
         response.reply("The dragon takes a deep breath and bathes #{name} in flame.")
+      end
+      
+      def bomb(response)
+        response.reply('http://korrektivpress.com/wp-content/uploads/2013/12/world-on-fire-the-end.jpg')
       end
     end
 
